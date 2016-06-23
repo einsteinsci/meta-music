081ca5d2249ab5556e72e538b412c0b6c8d75bae
sealedinterface <sealedinterface@gmail.com>
2016-06-22 17:21:10 -0700

    Moved UI functionality to overall logic class

--- MetaMusic/MetaMusic/MainWindow.xaml.cs
+++ MetaMusic/MetaMusic/MainWindow.xaml.cs
@@ -12,15 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Windows.Threading;
 
 using MahApps.Metro.Controls;
-
-using MetaMusic.Players;
-using MetaMusic.Sources;
-
-using UltimateUtil;
-using IOPath = System.IO.Path;
 
 namespace MetaMusic
 {
@@ -31,92 +24,26 @@ namespace MetaMusic
 	// ReSharper disable once RedundantExtendsListEntry
 	public partial class MainWindow : MetroWindow
 	{
-		public readonly MediaPlayer InternalFilePlayer = new MediaPlayer();
-		public readonly WebMusicHelper InternalWebPlayer = new WebMusicHelper();
-
-		public readonly DispatcherTimer Timer = new DispatcherTimer {
-			Interval = TimeSpan.FromSeconds(0.5)
-		};
-
-		public readonly VersatilePlayer Player;
-
-		private IMusicSource _currentTrack;
-
 		//private FlashWindowHelper _flashHelper = new FlashWindowHelper();
+
+		public MetaMusicLogic Logic
+		{ get; private set; }
 
 		public MainWindow()
 		{
 			InitializeComponent();
 
-			Player = new VersatilePlayer(InternalFilePlayer, InternalWebPlayer);
-
-			Timer.Tick += (s, e) => { StatusTxt.Text = Player.StatusText; };
-
-			Timer.Start();
-
-			Player.TitleChanged += (s, e) => { UpdateTitleBar(); };
-		}
-
-		public void UpdateTitleBar()
-		{
-			Dispatcher.Invoke(() => {
-				if (Player.Source != null)
-				{
-					Title = "MM | " + IOPath.GetFileName(Player.Source.Title);
-				}
-				else
-				{
-					Title = "Meta Music Player";
-				}
-			});
-		}
-
-		public void UpdatePlayBtn(string desc)
-		{
-			PlayBtn.Content = desc;
-			PlayThumbBtn.Description = desc;
-
-			if (desc == "Play" || desc == "Resume")
-			{
-				PlayThumbBtn.ImageSource = Util.LoadImage("Assets/play.png");
-			}
-			else
-			{
-				PlayThumbBtn.ImageSource = Util.LoadImage("Assets/pause.png");
-			}
+			Logic = new MetaMusicLogic(this);
 		}
 
 		private void PlayBtn_OnClick(object sender, RoutedEventArgs e)
 		{
-			if (Player.Source == null)
-			{
-				if (MusicPathBox.Text.StartsWith("http"))
-				{
-					_currentTrack = new SoundCloudMusic(MusicPathBox.Text, InternalWebPlayer);
-				}
-				else
-				{
-					_currentTrack = new FileMusic(MusicPathBox.Text, InternalFilePlayer);
-				}
-
-				Player.Play(_currentTrack);
-				UpdatePlayBtn("Pause");
-			}
-			else
-			{
-				Player.TogglePause();
-				UpdatePlayBtn(Player.Source.IsPlaying ? "Pause" : "Resume");
-			}
+			Logic.TogglePlay();
 		}
 
 		private void StopBtn_OnClick(object sender, RoutedEventArgs e)
 		{
-			//_scStop = true;
-			//InternalWebPlayer.Stop();
-
-			Player.Stop();
-			UpdateTitleBar();
-			UpdatePlayBtn("Play");
+			Logic.Stop();
 		}
 
 		private void PlayThumbBtn_OnClick(object sender, EventArgs e)
@@ -131,12 +58,7 @@ namespace MetaMusic
 
 		private void MainWindow_OnClosed(object sender, EventArgs e)
 		{
-			//if (StreamPlayer != null && StreamPlayer.IsBusy)
-			//{
-			//	StreamPlayer.CancelAsync();
-			//}
-
-			InternalWebPlayer.Stop();
+			Logic.ShutDown();
 		}
 	}
 }
---
+++ MetaMusic/MetaMusic/MetaMusicLogic.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+using MetaMusic.Players;
+using MetaMusic.Sources;
+
+namespace MetaMusic
+{
+	public class MetaMusicLogic
+	{
+		public MainWindow Window
+		{ get; private set; }
+
+		public Dispatcher Dispatcher => Window.Dispatcher;
+
+		public readonly MediaPlayer InternalFilePlayer = new MediaPlayer();
+		public readonly WebMusicHelper InternalWebPlayer = new WebMusicHelper();
+
+		public readonly DispatcherTimer Timer = new DispatcherTimer
+		{
+			Interval = TimeSpan.FromSeconds(0.5)
+		};
+
+		public readonly VersatilePlayer Player;
+
+		private IMusicSource _currentTrack;
+
+		public MetaMusicLogic(MainWindow window)
+		{
+			Window = window;
+
+			Player = new VersatilePlayer(InternalFilePlayer, InternalWebPlayer);
+
+			Timer.Tick += (s, e) => { Window.StatusTxt.Text = Player.StatusText; };
+
+			Timer.Start();
+
+			Player.TitleChanged += (s, e) => { UpdateTitleBar(); };
+		}
+
+		public void UpdateTitleBar()
+		{
+			Dispatcher.Invoke(() => {
+				if (Player.Source != null)
+				{
+					Window.Title = "MM | " + Path.GetFileName(Player.Source.Title);
+				}
+				else
+				{
+					Window.Title = "Meta Music Player";
+				}
+			});
+		}
+
+		public void UpdatePlayBtn(string desc)
+		{
+			Window.PlayBtn.Content = desc;
+			Window.PlayThumbBtn.Description = desc;
+
+			if (desc == "Play" || desc == "Resume")
+			{
+				Window.PlayThumbBtn.ImageSource = Util.LoadImage("Assets/play.png");
+			}
+			else
+			{
+				Window.PlayThumbBtn.ImageSource = Util.LoadImage("Assets/pause.png");
+			}
+		}
+
+		public void TogglePlay()
+		{
+			if (Player.Source == null)
+			{
+				if (Window.MusicPathBox.Text.StartsWith("http"))
+				{
+					_currentTrack = new SoundCloudMusic(Window.MusicPathBox.Text, InternalWebPlayer);
+				}
+				else
+				{
+					_currentTrack = new FileMusic(Window.MusicPathBox.Text, InternalFilePlayer);
+				}
+
+				Player.Play(_currentTrack);
+				UpdatePlayBtn("Pause");
+			}
+			else
+			{
+				Player.TogglePause();
+				UpdatePlayBtn(Player.Source.IsPlaying ? "Pause" : "Resume");
+			}
+		}
+
+		public void Stop()
+		{
+			Player.Stop();
+			UpdateTitleBar();
+			UpdatePlayBtn("Play");
+		}
+
+		public void ShutDown()
+		{
+			Stop();
+
+			Timer.Stop();
+			InternalWebPlayer.Stop();
+		}
+	}
+}
