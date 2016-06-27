775118c3266bbad66565db3cc3069403526f5ede
sealedinterface <sealedinterface@gmail.com>
2016-06-27 15:38:02 -0700

    Revert "Revert "Minimalist mode layout done""
    This reverts commit 56e2dbb3cf24aceaf34c3a8b3709850dd57d63c3.

--- MetaMusic/MetaMusic/MainWindow.xaml.cs
+++ MetaMusic/MetaMusic/MainWindow.xaml.cs
@@ -12,10 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 using MahApps.Metro.Controls;
-
-using MetaMusic.BrstmConvert;
 
 namespace MetaMusic
 {
@@ -26,16 +25,101 @@ namespace MetaMusic
 	// ReSharper disable once RedundantExtendsListEntry
 	public partial class MainWindow : MetroWindow
 	{
+		public const string MINIMALIST = "\x25be";
+		public const string NORMAL_WINDOW = "\x25c6";
+		public const string PREV = "\x00ab";
+		public const string NEXT = "\x00bb";
+		public const string PAUSE = "\x23f8";
+		public const string PLAY = "\x25b6";
+		public const string STOP = "\x25a0";
+		public const string SPEAKER_ON = "\uD83D\uDD08";
+		public const string SPEAKER_OFF = "\uD83D\uDD07";
+
+		public const int REDUCE_PROGRESS = 850;
+		public const int HIDE_PROGRESS = 700;
+		public const int HIDE_VOLUME = 550;
+		public const int HIDE_SONGNAME = 450;
+		public const int HIDE_CONTROLS = 320;
+
+		public const int PROGRESS_LARGE = 200;
+		public const int PROGRESS_SMALL = 100;
+
 		//private FlashWindowHelper _flashHelper = new FlashWindowHelper();
 
 		public MetaMusicLogic Logic
 		{ get; private set; }
+
+		public readonly DispatcherTimer DebugTimer;
+
+		public bool IsMinimalist
+		{ get; private set; }
+
+		public double LastHeight
+		{ get; private set; }
+
+		private readonly List<string> debugLines = new List<string>();
+
+		private bool _playMode;
+
+		private bool _muted;
 
 		public MainWindow()
 		{
 			InitializeComponent();
 
 			Logic = new MetaMusicLogic(this);
+
+			DebugTimer = new DispatcherTimer {
+				Interval = TimeSpan.FromSeconds(0.02)
+			};
+			DebugTimer.Tick += (s, e) => {
+				getDebugLines();
+				DebugTxt.Text = string.Join("\n", debugLines);
+				MMTxt.ToolTip = string.Join("\n", debugLines);
+			};
+		}
+
+		internal void getDebugLines()
+		{
+			debugLines.Clear();
+
+			debugLines.Add("Volume: " + Min_VolumeSlider.Value.ToString("F2"));
+			debugLines.Add("Width: " + Width);
+		}
+
+		public void ToggleMinimalist()
+		{
+			IsMinimalist = !IsMinimalist;
+
+			if (IsMinimalist)
+			{
+				LastHeight = Height;
+			}
+
+			MaxHeight = IsMinimalist ? 39.0 : double.PositiveInfinity;
+			Height = IsMinimalist ? 39.0 : LastHeight;
+			MinimalistBtn.Content = IsMinimalist ? NORMAL_WINDOW : MINIMALIST;
+			IsMaxRestoreButtonEnabled = !IsMinimalist;
+
+			MainGrid.Visibility = (!IsMinimalist).ToVis();
+			Min_FlowControlPanel.Visibility = IsMinimalist.ToVis();
+			Min_VolumeGrid.Visibility = IsMinimalist.ToVis();
+			Min_ProgressSlider.Visibility = IsMinimalist.ToVis();
+
+			UpdateMinimalistControlsVisibility(Width);
+		}
+
+		public void UpdateMinimalistControlsVisibility(double width)
+		{
+			Min_ProgressSlider.Width = width > REDUCE_PROGRESS ? PROGRESS_LARGE : PROGRESS_SMALL;
+
+			if (IsMinimalist)
+			{
+				Min_ProgressSlider.Visibility = (width > HIDE_PROGRESS).ToVis();
+				Min_VolumeGrid.Visibility = (width > HIDE_VOLUME).ToVis();
+				Title = width > HIDE_SONGNAME ? "Captain-Cool.mp3" : "";
+				Min_FlowControlPanel.Visibility = (width > HIDE_CONTROLS).ToVis();
+			}
 		}
 
 		private void PlayBtn_OnClick(object sender, RoutedEventArgs e)
@@ -66,13 +150,58 @@ namespace MetaMusic
 		private void MainWindow_OnClosed(object sender, EventArgs e)
 		{
 			Logic.ShutDown();
+			DebugTimer.Stop();
 		}
 
-		private void DoInCubeStuffBtn_OnClick(object sender, RoutedEventArgs e)
+		private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
 		{
-			string outputPath = VgmstreamConverter.ConvertToWAV("victim.brstm");
-			StatusTxt.Text = outputPath;
-			Logic.Timer.Stop();
+			Logic.UpdatePlayBtn(_playMode ? "Pause" : "Play");
+			DebugTimer.Start();
+		}
+
+		private void MainWindow_OnSizeChanged(object sender, SizeChangedEventArgs e)
+		{
+			if (!e.WidthChanged)
+			{
+				return;
+			}
+
+			double width = e.NewSize.Width;
+
+			UpdateMinimalistControlsVisibility(width);
+		}
+
+		private void PrevThumbBtn_OnClick(object sender, EventArgs e)
+		{ }
+
+		private void NextThumbBtn_OnClick(object sender, EventArgs e)
+		{ }
+
+		private void MinimalistBtn_OnClick(object sender, RoutedEventArgs e)
+		{
+			ToggleMinimalist();
+		}
+
+		private void Min_PrevTrackBtn_OnClick(object sender, RoutedEventArgs e)
+		{ }
+
+		private void Min_PlayBtn_OnClick(object sender, RoutedEventArgs e)
+		{
+			_playMode = !_playMode;
+			Logic.UpdatePlayBtn(_playMode ? "Pause" : "Play");
+		}
+
+		private void Min_StopBtn_OnClick(object sender, RoutedEventArgs e)
+		{ }
+
+		private void Min_NextTrackBtn_OnClick(object sender, RoutedEventArgs e)
+		{ }
+
+		private void Min_VolumeBtn_OnClick(object sender, RoutedEventArgs e)
+		{
+			_muted = !_muted;
+			Min_VolumeBtn.Content = _muted ? SPEAKER_OFF : SPEAKER_ON;
+			Min_VolumeBtn.Margin = new Thickness { Right = _muted ? 5 : 16, Left = _muted ? 0 : 2, Bottom = 3 };
 		}
 	}
 }
--- MetaMusic/MetaMusic/MetaMusicLogic.cs
+++ MetaMusic/MetaMusic/MetaMusicLogic.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Media;
 using System.Windows.Threading;
 
@@ -38,7 +39,9 @@ namespace MetaMusic
 
 			Player = new VersatilePlayer(FileMediaPlayer, WebHelper, WavHelper);
 
-			Timer.Tick += (s, e) => { Window.StatusTxt.Text = Player.LoadingText; };
+			Timer.Tick += (s, e) => {
+				//Window.StatusTxt.Text = Player.LoadingText;
+			};
 
 			Timer.Start();
 
@@ -61,10 +64,12 @@ namespace MetaMusic
 
 		public void UpdatePlayBtn(string desc)
 		{
-			Window.PlayBtn.Content = desc;
+			//Window.PlayBtn.Content = desc;
 			Window.PlayThumbBtn.Description = desc;
+			Window.Min_PlayBtn.ToolTip = desc;
 
-			if (desc == "Play" || desc == "Resume")
+			bool isPlay = (desc == "Play" || desc == "Resume");
+			if (isPlay)
 			{
 				Window.PlayThumbBtn.ImageSource = Util.LoadImage("Assets/play.png");
 			}
@@ -72,13 +77,26 @@ namespace MetaMusic
 			{
 				Window.PlayThumbBtn.ImageSource = Util.LoadImage("Assets/pause.png");
 			}
+
+			if (isPlay)
+			{
+				Window.Min_PlayBtn.Content = MainWindow.PLAY;
+				Window.Min_PlayBtn.FontSize = 16;
+				Window.Min_PlayBtn.Margin = new Thickness { Bottom = 3, Left = 5, Right = 5 };
+			}
+			else
+			{
+				Window.Min_PlayBtn.Content = MainWindow.PAUSE;
+				Window.Min_PlayBtn.FontSize = 20;
+				Window.Min_PlayBtn.Margin = new Thickness { Bottom = 5, Left = 2, Right = 1.5 };
+			}
 		}
 
 		public void TogglePlay()
 		{
 			if (Player.Source == null)
 			{
-				string url = Window.MusicPathBox.Text;
+				string url = "";//Window.MusicPathBox.Text;
 
 				if (url.StartsWith("https://soundcloud.com") || url.StartsWith("http://soundcloud.com"))
 				{
